dca204c370c4572b73db5998cb0d186e20d2cb8a
yinger <75009049+yingerliang@users.noreply.github.com>
2022-04-28 21:11:21 +1200

    Added portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,85 @@
 import './portfolio.css'
+import weatherapp from '../../assets/weatherapp.png'
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={weatherapp} alt="Weather App" />
+          </div>
+
+          <h3>React Weather App</h3>
+          <a
+            href="https://github.com/yingerliang/react-weather-app"
+            className="btn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="https://react-weather-app-becca.netlify.app/"
+            className="btn btn-primary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={weatherapp} alt="Weather App" />
+          </div>
+
+          <h3>React Weather App</h3>
+          <a
+            href="https://github.com/yingerliang/react-weather-app"
+            className="btn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="https://react-weather-app-becca.netlify.app/"
+            className="btn btn-primary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={weatherapp} alt="Weather App" />
+          </div>
+
+          <h3>React Weather App</h3>
+          <a
+            href="https://github.com/yingerliang/react-weather-app"
+            className="btn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="https://react-weather-app-becca.netlify.app/"
+            className="btn btn-primary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Portfolio
