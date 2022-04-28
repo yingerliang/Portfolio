f151b6453dbe27783b8cba40ac5fdf32f668df2c
yinger <75009049+yingerliang@users.noreply.github.com>
2022-04-28 22:05:05 +1200

    Fixed header responsiveness

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,13 +10,13 @@ const Header = () => {
         <h5>Hello, I'm</h5>
         <h1>Rebecca</h1>
         <h5>Frontend Developer based in New Zealand</h5>
-        <CTA />
-        <HSocials />
 
         <div className="header_logo">
-          <img src={logo} alt="Header Image" className="logo" />
+          <img src={logo} alt="Header" className="logo" />
         </div>
 
+        <CTA />
+        <HSocials />
         <a href="#contact" className="scroll_down">
           Scroll Down
         </a>
